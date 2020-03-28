1bb0e72164b2e2fb399ae3538907dbf1ba6a082b
robycrystal <62775647+robycrystal@users.noreply.github.com>
2020-03-28 02:35:16 +0100

    Add files via upload

---
+++ Bridge Girl Game/Assets/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float x;
+    public float z;
+    public float speed;
+    public Rigidbody Rigid;
+    public float Rotation;
+    public bool isOnGround;
+    public float jumpForce;
+    public int health = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving();
+        Jumping();
+        Debug.Log(health);
+    }
+
+    void moving()
+    {
+        z = Input.GetAxis("Vertical") * speed;
+       
+
+        z *= Time.deltaTime;
+       
+
+        Vector3 mouvment = new Vector3(x, 0, z);
+
+        Rigid.AddForce(mouvment * speed * Time.deltaTime);
+
+        //Go left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) )
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        //Go right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) )
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        // Movement of translation along the object's z-axis
+        transform.Translate(x, 0, z);
+
+     
+    }
+
+    public void Jumping()
+    {
+
+        if (Input.GetKey(KeyCode.Space) && isOnGround)
+        {
+            Rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            Debug.Log("jump");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isOnGround = true;
+
+        }
+    }
+}
