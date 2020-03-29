ff4ff3014b3f5152d0774ca7c3cf48d6fcb86686
Monolilith <43307776+Monolilith@users.noreply.github.com>
2020-03-29 10:08:09 -0700

    Canvas UI + Some Programming
    It's here!

---
+++ Bridge Girl Game/Assets/Scripts/SliderValueChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueChange : MonoBehaviour
+{
+
+    public int currentVal;
+    public int newVal;
+
+    private Slider bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MinusOne()
+    {
+        Minus();
+    }
+
+    public IEnumerator Minus()
+    {
+        for(float i = currentVal; i > newVal; i-= 0.1f)
+        {
+            bar.value = i;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
